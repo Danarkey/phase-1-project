317b4ba14f806bf20884d5a8eaf0cf0a006ad74f
Danarkey <138664486+Danarkey@users.noreply.github.com>
2023-08-28 14:44:59 +0800

    Stats table colour value adjust

--- script.js
+++ script.js
@@ -1,58 +1,76 @@
 const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-    const calculateBarWidth = (value) => (value / 255) * 100;
-    const calculateBarColor = (value) => {
-      if (value > 150) {
-        return "#52ff00"; // Green
-      } else if (value > 100) {
-        return "#ccff00"; // Yellow
-      } else {
-        return "#fffe00"; // Red
-      }
-    };
+const calculateBarWidth = (value) => (value / 255) * 100;
+const calculateBarColor = (value) => {
+    console.log("Value:", value);
+    if (value >= 120) {
+      return "#4eff00"; // Bright Green
+    } else if (value >= 100) {
+      return "#52ff00"; // Green
+    } else if (value === 100) {
+      return "#ccff00"; // Yellow
+    } else if (value >= 90) {
+      return "#ffe400"; // Yellow
+    } else if (value >= 80) {
+      return "#ffac00"; // Orange
+    } else if (value >= 70) {
+      return "#ff6600"; // Orange
+    } else if (value >= 60) {
+      return "#ff4c00"; // Dark Orange
+    } else {
+      return "#ff0400"; // Red
+    }
+  };
+  
+  
+const updateStats = (selectedPokemon) => {
+  fetch(apiUrl + selectedPokemon)
+    .then((response) => response.json())
+    .then((data) => {
+      const stats = data.stats;
 
-    const updateStats = (selectedPokemon) => {
-      fetch(apiUrl + selectedPokemon)
-        .then((response) => response.json())
-        .then((data) => {
-          const stats = data.stats;
+      stats.forEach((stat) => {
+        const statName = stat.stat.name;
+        const baseStat = stat.base_stat;
 
-          stats.forEach((stat) => {
-            const statName = stat.stat.name;
-            const baseStat = stat.base_stat;
+        const statValueElement = document.getElementById(`${statName}-value`);
+        const statBarElement = document.getElementById(`${statName}-bar`);
 
-            const statValueElement = document.getElementById(`${statName}-value`);
-            const statBarElement = document.getElementById(`${statName}-bar`);
-
-            statValueElement.textContent = baseStat;
-            statBarElement.style.width = `${calculateBarWidth(baseStat)}%`;
-            statBarElement.style.backgroundColor = calculateBarColor(baseStat);
-          });
-        })
-        .catch((error) => {
-          console.error("Error fetching API data:", error);
-        });
-    };
-
-    const searchInput = document.getElementById("pokemon-search");
-    const autocompleteDropdown = document.getElementById("autocomplete-dropdown");
-    const awesomplete = new Awesomplete(searchInput, {
-      list: [],
-      minChars: 2, // Adjust the minimum characters required for suggestions
+        statValueElement.textContent = baseStat;
+        statBarElement.style.width = `${calculateBarWidth(baseStat)}%`;
+        statBarElement.style.backgroundColor = calculateBarColor(baseStat);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching API data:", error);
     });
+};
 
-    searchInput.addEventListener("input", (event) => {
-      const searchText = event.target.value;
-      fetch(apiUrl + "?limit=10&offset=0")
-        .then((response) => response.json())
-        .then((data) => {
-          const pokemonList = data.results.map((pokemon) => pokemon.name);
-          const filteredList = pokemonList.filter((pokemon) =>
-            pokemon.includes(searchText.toLowerCase())
-          );
-          awesomplete.list = filteredList;
-        })
-        .catch((error) => {
-          console.error("Error fetching API data:", error);
-        });
+const searchInput = document.getElementById("pokemon-search");
+const datalist = document.getElementById("pokemon-options");
+
+searchInput.addEventListener("input", (event) => {
+  const searchText = event.target.value.toLowerCase();
+  datalist.innerHTML = "";
+
+  fetch(apiUrl + `?limit=10&offset=0`)
+    .then((response) => response.json())
+    .then((data) => {
+      const pokemonList = data.results.map((pokemon) => pokemon.name);
+      const filteredList = pokemonList.filter((pokemon) => pokemon.includes(searchText));
+
+      filteredList.forEach((pokemon) => {
+        const option = document.createElement("option");
+        option.value = pokemon;
+        datalist.appendChild(option);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching API data:", error);
     });
+});
+
+searchInput.addEventListener("change", (event) => {
+  const selectedPokemon = event.target.value.toLowerCase();
+  updateStats(selectedPokemon);
+});
